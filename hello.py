4d40ba03faf73d0e4f9286ec20357d156d9c3270
Harsh645089 <Harsh24235@iiitd.ac.in>
2024-08-23 17:22:45 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,3 @@
 <<<<<<< HEAD
 print("Hello World!")
-print("Sarvjeet contibuted this code.....")
-=======
-# Run the script
-print("Hello World!")
->>>>>>> 84b766aa5d06de2351f85c3898b6629b3835c805
+print("i can do better than this!")
